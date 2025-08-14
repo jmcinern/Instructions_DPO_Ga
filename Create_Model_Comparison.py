fd342c46cba1913484851d840df6e6be07406063
joseph <jmcinern@tcd.ie>
2025-08-14 10:56:45 +0100

    get snippets for test

--- Create_Model_Comparison.py
+++ Create_Model_Comparison.py
@@ -8,6 +8,7 @@ df = df[df["lang"] == "ga"]
 text_list = df["text"].tolist()
 # filter by >100 chars
 text_list = [text for text in text_list if len(text) > 100]
+print(f"Number of texts: {len(text_list)}")
 # save to txt
 with open("ga_texts.txt", "w", encoding="utf-8") as f:
     f.writelines("\n".join(text_list))
